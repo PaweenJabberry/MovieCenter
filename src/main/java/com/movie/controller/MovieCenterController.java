36347f6a52fec65bd12c9db4a95af58ed26e9675
JabBerry <33551495+PaweenJabberry@users.noreply.github.com>
2020-03-27 17:26:23 +0700

    Update v.1.5
    - Create ShowMovies as controller for show all Movies on view page

--- src/main/java/com/movie/controller/MovieCenterController.java
+++ src/main/java/com/movie/controller/MovieCenterController.java
@@ -1,13 +1,34 @@
 package com.movie.controller;
 
+import com.movie.repositories.MovieRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MovieCenterController {
 
+    private final MovieRepository movieRepository;
+
+    public MovieCenterController(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    //  http://localhost:8080/
     @RequestMapping("/")
     public String index() {
         return "web/index";
+    }
+
+    //  http://localhost:8080/movies
+    @RequestMapping("/movies")
+    public String showAllMovies(Model model) {
+        model.addAttribute("movies", movieRepository.findAll());
+        return "service/list";
+    }
+
+    @RequestMapping("/search")
+    public String searchByName() {
+        return "service/list";
     }
 }
